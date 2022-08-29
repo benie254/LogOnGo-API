648271511566741bba13b05ebd9d7385d167939e
benie254 <davinci.monalissa@gmail.com>
2022-08-29 19:40:08 +0300

    update index template & views

--- log_app/views.py
+++ log_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404, render,redirect
 
-from log_app.models import Fuel 
+from log_app.models import Fuel, Log, LogMpesa, MyUser 
 
 def home(request):
     fuels = Fuel.objects.all()
+    users = MyUser.objects.all()
+    logs = Log.objects.all()
+    mpesa = LogMpesa.objects.all()
     print(fuels)
-    return render(request,'index.html',{"fuels":fuels,})
+    return render(request,'index.html',{"fuels":fuels,"users":users,"logs":logs})
