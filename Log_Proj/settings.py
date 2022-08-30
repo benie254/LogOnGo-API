077bc294d849eb5278ea7fd039e9a8b4324d0ae2
benie254 <davinci.monalissa@gmail.com>
2022-08-30 06:23:39 +0300

    update app

--- Log_Proj/settings.py
+++ Log_Proj/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
