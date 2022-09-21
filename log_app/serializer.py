1cb02964410600503bbe95a971a07092df0ccf54
benie254 <davinci.monalissa@gmail.com>
2022-09-21 07:47:28 +0300

    serialize new fields

--- log_app/serializer.py
+++ log_app/serializer.py
@@ -30,7 +30,7 @@ class LogSerializer(serializers.ModelSerializer):
 class LogMpesaSerializer(serializers.ModelSerializer):
     class Meta:
         model = LogMpesa 
-        fields = ('id','date','transaction_number','user','logged_by','customer_name','customer_phone_number')
+        fields = ('id','date','transaction_number','user','logged_by','customer_name','customer_phone_number','amount','amount_transferred_to_bank')
 
 class FuelReceivedSerializer(serializers.ModelSerializer):
     class Meta:
