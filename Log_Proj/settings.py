0b43ea30e6b0385e9ea56fcceda50870cad0daed
benie254 <davinci.monalissa@gmail.com>
2022-10-02 13:30:53 +0300

    remove unused settings

--- Log_Proj/settings.py
+++ Log_Proj/settings.py
@@ -84,8 +84,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
      'rest_framework_simplejwt.authentication.JWTAuthentication',
      ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,
 }
 
 JWT_AUTH = {
